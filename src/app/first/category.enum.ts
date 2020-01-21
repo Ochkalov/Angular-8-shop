121bece28043a52016d1854070bcf9f0295c3145
Yurii Ochkalov <ochkalov@gmail.com>
2020-01-21 22:07:38 +0200

    HomW1: Task-4

---
+++ src/app/first/category.enum.ts
@@ -0,0 +1,5 @@
+export enum Category {
+  Books,
+  App,
+  Phone
+}
--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Category} from './category.enum';
 
 @Component({
   selector: 'app-first',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first.component.less']
 })
 export class FirstComponent implements OnInit {
+
+  name: string = 'Yurii';
+  description: string = 'Shop For Yurii';
+  price: number = 100;
+  category: Category;
+  isAvailable: boolean = true;
+  arr: number[] = [1, 2, 3, 4, 5];
+  arrStr: string[] = ['a', 'b', 'c'];
 
   constructor() { }
 
